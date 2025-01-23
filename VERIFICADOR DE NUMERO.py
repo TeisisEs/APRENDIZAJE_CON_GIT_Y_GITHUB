9c23f78a46553177a1cbd5eca50b5b44091c5cb6
TeisisEs <escobar.teisis059@gmail.com>
2025-01-23 13:57:46 -0600

    Esta es la nueva prueba del comando para hacer un git add y un commit al mismo tiempo

---
+++ VERIFICADOR DE NUMERO.py
@@ -0,0 +1,20 @@
+#PROGRAMA QUE VERIFICA SI UN NUMERO ES PAR O NO
+print("=" * 50)
+print("            VERIFICADOR SI ES NUMERO PAR O IMPAR")
+print("=" * 50)
+
+entrada_numero = float(input("INGRESA TU NUMERO A VERIFICAR: "))
+
+variable  = entrada_numero
+
+if variable % 2 == 0 :
+    print("El numero es PAR")
+
+else:
+
+    print("El numero es IMPAR")
+    
+print("ESTA ES LA PRIMERA PRUEBA DE GIT")
+
+
+
